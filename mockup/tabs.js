8879b88ce8cd042f59d93be878360f00ccac80df
jackoe <jackwines@mac.com>
2017-10-02 16:59:23 -0500

    changed open city for open tab

--- mockup/tabs.js
+++ mockup/tabs.js
@@ -1,4 +1,4 @@
-function openCity(evt, cityName) {
+function openTab(evt, tabName) {
     // Declare all variables
     var i, tabcontent, tablinks;
 
@@ -13,10 +13,8 @@ function openCity(evt, cityName) {
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-
     // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
 
-openCity(event, 'Home')

21b68e318f0b0367d22d3efac484a643fbf57f97
jackoe <jackwines@mac.com>
2017-10-10 14:18:01 -0500

    added some more js and a submit button

---
+++ web/static/attendance.js
@@ -0,0 +1,9 @@
+function sendSubmitForm()  {
+    theirText = document.getElementById("someRandoText").value
+    console.log(theirText)
+    /*
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+    xhr.send(someStuff);
+    */
+}

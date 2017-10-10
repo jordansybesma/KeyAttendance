91bbef5885471479b7510753d57c3f8992d6058e
Russell Smith <Russll@eduroam-160-74.dyn.carleton.edu>
2017-10-10 14:35:43 -0500

    work on attendance oython

--- web/addAttendance.py
+++ web/addAttendance.py
@@ -1,1 +1,19 @@
 #sample function to make attendance sheet in database
+
+
+import flask
+import json
+import psycopg2
+import sys
+
+
+def addAttendee(someJSON):
+    convertedJSON = json.loads(someJSON)
+    print(convertedJSON["text"])
+    
+
+
+
+
+if __name__ == '__main__':
+    addAttendee('{"text": "AAAAAAAAHHH"}')

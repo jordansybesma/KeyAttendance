3330c32de56828376453b5e1c360237a8cf678f8
Lindsey Chavez <chavezl@carleton.edu>
2017-10-10 14:45:48 -0500

    updated attendance.js hopefully working post request

--- web/static/attendance.js
+++ web/static/attendance.js
@@ -6,4 +6,20 @@ function sendSubmitForm()  {
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
     xhr.send(someStuff);
     */
+
+
+    // Sending and receiving data in JSON format using POST method
+    //
+    var xhr = new XMLHttpRequest();
+    var url = "137.22.166.165/addText/";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var json = JSON.parse(xhr.responseText);
+            console.log(json.email + ", " + json.password);
+        }
+    };
+    var data = JSON.stringify({"text": theirText});
+    xhr.send(data);
 }

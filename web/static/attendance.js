9d093a5babb4b1886254dc717b2ee94d00407399
jackoe <jackwines@mac.com>
2017-10-19 14:07:08 -0500

    js now has talk to back-end function

--- web/static/attendance.js
+++ web/static/attendance.js
@@ -1,22 +1,16 @@
+function sendRequest(isPost, data, header, value, urlAddOn)  {
+    var xhr = new XMLHttpRequest();
+    xhr.open(isPost? "POST": "GET", urlAddOn, true);
+    xhr.setRequestHeader(header, value);
+    var data = JSON.stringify({"text": theirText});
+    xhr.send(data);
+    return xhr.responseText;
+}
+
+
 function sendSubmitForm()  {
     theirText = document.getElementById("someRandoText").value
     console.log(theirText)
-    console.log(window.location.hostname)
-    /*
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-    xhr.send(someStuff);
-    */
 
-
-    // Sending and receiving data in JSON format using POST method
-    //
-    var xhr = new XMLHttpRequest();
-    var url = "137.22.166.165/addText/";
-    url = "http://127.0.0.1:5000/addText/";
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("attendance-json", "application/json");
-    
-    var data = JSON.stringify({"text": theirText});
-    xhr.send(data);
+    console.log('theirText:' + sendRequest(true, theirText, "attendance-json", "application/json", "/addText"));
 }

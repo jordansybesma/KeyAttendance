ee70186a7f367791da7d44909c30cdf73c2009ea
Russell Smith <Russll@eduroam-160-74.dyn.carleton.edu>
2017-10-24 13:54:22 -0500

    autofill formatting

--- web/static/attendance.js
+++ web/static/attendance.js
@@ -1,7 +1,7 @@
 function sendRequest(isPost, data, header, value, urlAddOn)  {
     var xhr = new XMLHttpRequest();
     xhr.open(isPost? "POST": "GET", urlAddOn, true);
-    xhr.setRequestHeader(header, value);
+    // xhr.setRequestHeader(header, value);
     var data = JSON.stringify({"text": theirText});
     xhr.send(data);
     return xhr.responseText;

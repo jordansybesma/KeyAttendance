459d1c43c86e28b69f24a976b04caff359596853
rachelmoore212 <moorer2@carleton.edu>
2017-10-26 12:55:28 -0500

    css changes

--- web/static/attendance.js
+++ web/static/attendance.js
@@ -141,6 +141,7 @@ function showStudentProfile() {
     }
     if (optionFound) {
         alert("option found")
+        alert(keywordElement)
         getRequest("http://ec2-35-160-216-144.us-west-2.compute.amazonaws.com:5000/getID/" + keywordElement, "", showProfile);
 
 

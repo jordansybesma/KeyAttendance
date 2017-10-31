0db1ac3a51ace7294cc695a0e8c2cdc357ca6e58
rachelmoore212 <moorer2@carleton.edu>
2017-10-31 14:42:30 -0500

    stuff

--- web/static/attendance.js
+++ web/static/attendance.js
@@ -179,6 +179,7 @@ function onAddRow() {
 }
 
 function displayAttendanceTable(table_name) {
+    alert("got here");
     var popUp = document.getElementById('attendanceDiv');
     popUp.style.display = "block";
     var list = document.getElementById('attendanceListDiv');

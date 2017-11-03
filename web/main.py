fab610964210e8576bf5745be04b43e1c8df0954
rachelmoore212 <moorer2@carleton.edu>
2017-11-03 17:57:21 -0500

    working on adding students

--- web/main.py
+++ web/main.py
@@ -171,7 +171,7 @@ def addAttendant():
         result = json.dumps(executeSingleQuery(queryMaster,fetch = True))
         newResult =json.loads(result)
     
-        if result is None:
+        if result == []:
             newQuery = "INSERT INTO masterAttendance VALUES('" + date + "', '1', '0', '0', '0', '0', '0', '0', '0', '0');"
             executeSingleQuery(newQuery, [])
         else:

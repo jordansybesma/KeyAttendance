4701ec53d096d6933b8a71f4775a6435f015576d
rachelmoore212 <moorer2@carleton.edu>
2017-11-03 09:59:09 -0500

    working on adding students

--- web/static/attendance.js
+++ web/static/attendance.js
@@ -308,6 +308,7 @@ function createNewAttendance() {
     //getRequest("/getAttendance/" + table_date, "", fillAttendance);??
 }
 
+
 function displayAttendanceTable(table_date) {
     var table = document.getElementById("Attendance-Table");
     table.innerHTML = "";
@@ -315,9 +316,21 @@ function displayAttendanceTable(table_date) {
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    var cell6 = row.insertCell(5);
+    var cell7 = row.insertCell(6);
+    var cell8 = row.insertCell(7);
+    var cell9 = row.insertCell(8);
     cell1.innerHTML = "Name";
-    cell2.innerHTML = "Health";
-    cell3.innerHTML = "Food";
+    cell2.innerHTML = "Art";
+    cell3.innerHTML = "Made Food";
+    cell4.innerHTML = "Recieved Food";
+    cell5.innerHTML = "Leadership";
+    cell6.innerHTML = "Excersize";
+    cell7.innerHTML = "Mental Health";
+    cell8.innerHTML = "Volunteering";
+    cell9.innerHTML = "One On One";
     var readable = makeDateReadable(table_date);
     var sql = makeDateSQL(readable);
     console.log(sql);

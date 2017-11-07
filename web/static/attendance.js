337e8c3bab2cf82372bcf0fd0c656ca248dba73c
jackoe <jackwines@mac.com>
2017-11-06 21:55:39 -0600

    a bunch of stuff

--- web/static/attendance.js
+++ web/static/attendance.js
@@ -208,9 +208,13 @@ function showStudentAttendance(_, data) {
     console.log(JSON.parse(data));
 
 }
+
+function fillProfileTable()  {
+
+}
+
 function addRowHelper(first, last, art, madeFood, recievedFood, leadership, exersize, mentalHealth, volunteering, oneOnOne) {
     var table = document.getElementById("Attendance-Table");
-    var keywordElement = document.getElementById('keyword').value;
 
     //var date = getCurrentDate();
     var date = document.getElementById("storeDate").innerHTML;
@@ -218,14 +222,15 @@ function addRowHelper(first, last, art, madeFood, recievedFood, leadership, exer
     var fields = ['art','madeFood','recievedFood','leadership','exersize','mentalHealth','volunteering','oneOnOne']
     var checked = [art, madeFood, recievedFood, leadership, exersize, mentalHealth, volunteering, oneOnOne]
     var row = table.insertRow(1);
-    row.insertCell(0).innerHTML = first + " " + last;
+    fullName = first + " " + last;
+    row.insertCell(0).innerHTML = fullName;
     
     for(var i = 0; i < 8; i++)  {
-        var str = "<input type=\"checkbox\" " + (checked[i]? "checked": "") + " onclick=\"selectActivity('" + keywordElement + "','" + fields[i] + "', '" + date + "')\">";
+        var str = "<input type=\"checkbox\" " + (checked[i]? "checked": "") + " onclick=\"selectActivity('" fullName "','" + fields[i] + "', '" + date + "')\">";
         row.insertCell(i + 1).innerHTML = str;
     }
     
-    var str = "<button type=\"button\" onclick=\"deleteAttendant('" + date + "', '" + keywordElement + "')\">Delete </button>"
+    var str = "<button type=\"button\" onclick=\"deleteAttendant('" + date + "', '" + fullName + "')\">Delete </button>"
     row.insertCell(9).innerHTML = str;
 }
 

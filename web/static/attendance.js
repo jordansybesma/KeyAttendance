5dbebcfd8b03c256252da1f3adc058c4ccc76a31
jackoe <jackwines@mac.com>
2017-11-07 13:40:11 -0600

    swapped it to a for loop

--- web/static/attendance.js
+++ web/static/attendance.js
@@ -391,24 +391,10 @@ function displayAttendanceTable(table_date) {
     var table = document.getElementById("Attendance-Table");
     table.innerHTML = "";
     var row = table.insertRow(0);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
-    var cell5 = row.insertCell(4);
-    var cell6 = row.insertCell(5);
-    var cell7 = row.insertCell(6);
-    var cell8 = row.insertCell(7);
-    var cell9 = row.insertCell(8);
-    cell1.innerHTML = "Name";
-    cell2.innerHTML = "Art";
-    cell3.innerHTML = "Made Food";
-    cell4.innerHTML = "Recieved Food";
-    cell5.innerHTML = "Leadership";
-    cell6.innerHTML = "Exersize";
-    cell7.innerHTML = "Mental Health";
-    cell8.innerHTML = "Volunteering";
-    cell9.innerHTML = "One On One";
+    cellNames = ["Name", "Art", "Made Food", "Recieved Food", "Leadership", "Exersize", "Mental Health", "Volunteering", "One On One"];
+    for (header of cellNames) {
+        row.insertCell(-1).innerHTML = header;
+    }
     var readable = makeDateReadable(table_date);
     var sql = makeDateSQL(readable);
     document.getElementById("attendanceName").innerHTML = "Attendance Sheet " +readable;

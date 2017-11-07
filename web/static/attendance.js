5ae514e0e79a22d626d493a9acfaae9bde485ce7
Russell Smith <Russll@eduroam-160-74.dyn.carleton.edu>
2017-11-06 22:54:32 -0600

    working on displaying plot

--- web/static/attendance.js
+++ web/static/attendance.js
@@ -211,7 +211,7 @@ function showStudentAttendance(_, data) {
     // var x = [];
 
     for(i = 0; i < data.length; i++) {
-      var dateString = data[i][12];
+      var dateString = data[i];
       console.log(dateString);
       var dateList = dateString.split("-")
       var myDate = new date(parseInt(dateList[0]), parseInt(dateList[1]), parseInt(dateList[2]), 1, 1, 1, 1);

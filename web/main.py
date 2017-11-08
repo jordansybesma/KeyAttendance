77f966cf46612c46b1e251e0fdbfa6ed5bfe9a09
rachelmoore212 <moorer2@carleton.edu>
2017-11-08 10:18:38 -0600

    moving things around

--- web/main.py
+++ web/main.py
@@ -104,7 +104,7 @@ def getMasterAttendanceDate(dates):
     start = dateList[0]
     end = dateList[1]
     return json.dumps(executeSingleQuery("SELECT DISTINCT * FROM masterAttendance WHERE date >= '" + start + "' AND date <= '" + end + "' ORDER BY date DESC;",
-        fetch = True)[:10], indent=4, sort_keys=True, default=str)
+        fetch = True)[:8], indent=4, sort_keys=True, default=str)
         
 
 

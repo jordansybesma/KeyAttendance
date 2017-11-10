ef54317724da2f5ed2d6b1b7d300270d78f2cb77
rachelmoore212 <moorer2@carleton.edu>
2017-11-09 20:09:54 -0600

    refactoring graph

--- web/static/attendance.js
+++ web/static/attendance.js
@@ -206,7 +206,7 @@ function showStudentManageHelper(_, data) {
         console.log(myData[i]);
         var row = table.insertRow(-1);
         
-        row.insertCell(-1).innerHTML = makeHeaderReadable(myData[i][2]);
+        row.insertCell(-1).innerHTML = myData[i][2];
         var str = "<input type=\"checkbox\" "
             + (myData[i][0] ? "checked" : "")
             + " onclick=\"selectStudentColumn('" + myData[i][2] + "', 'isShowing')\">";

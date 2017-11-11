c804ed05a87e640eb727c193b0c41e5193dd6eb5
Russell Smith <Russll@eduroam-160-74.dyn.carleton.edu>
2017-11-11 08:50:01 -0600

    working on frequentPeers

--- web/main.py
+++ web/main.py
@@ -541,13 +541,13 @@ def frequentPeers(string):
     # print(databaseResult)
     # result = str(databaseResult)
     # return(result)
-    result = json.dumps(executeSingleQuery(query, fetch = True), indent=4, sort_keys=True, default=str)
+    result = json.dumps(executeSingleQuery(query, fetch = True), indent=4, sort_keys=True, default=list)
 
     for i in range(1, len(result)):
         print(type(result))
         print(result)
 
-    return result
+    return str(result)
 
 @app.route('/studentProfile/<string>')
 def studentProfile(string):

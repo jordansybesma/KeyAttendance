f22f11b037181ec71b751729137514a0928967c5
rachelmoore212 <moorer2@carleton.edu>
2017-11-10 20:43:05 -0600

    display student info

--- web/static/attendance.js
+++ web/static/attendance.js
@@ -480,7 +480,7 @@ function openEditProfile() {
     }
     var returnButton = document.createElement('button');
     returnButton.setAttribute('name', 'Return to Profile');
-    returnButton.setAttribute('onclick', '\"returnToProfile()\"');
+    returnButton.setAttribute('onclick', 'returnToProfile()');
     returnButton.innerHTML = "Return to Profile";
     div.appendChild(returnButton);
 }

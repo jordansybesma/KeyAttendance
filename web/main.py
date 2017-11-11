042234ac9b6b0f59ec64f853fa2a3a0b7bdbfa95
rachelmoore212 <moorer2@carleton.edu>
2017-11-11 10:52:26 -0600

    display student info

--- web/main.py
+++ web/main.py
@@ -68,8 +68,10 @@ def updateStudentInfo():
     value = request.form.get('value')
     if (value == "TRUE"):
         curVal = json.dumps(executeSingleQuery("SELECT " + column + " FROM testStudents WHERE firstname = '" + first + "' and lastName = '" + last + "';", []))
+        newResult =json.loads(curVal)
+        isChecked = newResult[0][0]
         print(curVal)
-        if "true" in curVal:
+        if (isChecked):
             query = "UPDATE testStudents SET "+ column + " = 'FALSE' WHERE firstName = '" + first + "' AND lastName = '" + last + "';"
         else:
             query = "UPDATE testStudents SET "+ column + " = 'TRUE' WHERE firstName = '" + first + "' AND lastName = '" + last + "';"

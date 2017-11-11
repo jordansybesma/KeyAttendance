dfd2cf2595c17d0a71caad6db03e15d9461ff99f
Russell Smith <Russll@eduroam-160-74.dyn.carleton.edu>
2017-11-11 08:51:12 -0600

    working on frequentPeers

--- web/main.py
+++ web/main.py
@@ -542,6 +542,7 @@ def frequentPeers(string):
     # result = str(databaseResult)
     # return(result)
     result = json.dumps(executeSingleQuery(query, fetch = True), indent=4, sort_keys=True, default=list)
+    print(result)
 
     for i in range(1, len(result)):
         print(type(result))

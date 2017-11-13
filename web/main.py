28e0a5ba5b85de4d4359960e7bad9bb5184e21fe
festeringalfalfah <festeringalfalfa@bitbucket.org>
2017-11-12 18:08:33 -0600

    alertawerkin'

--- web/main.py
+++ web/main.py
@@ -635,7 +635,7 @@ def addAlert():
 def checkAlert():
     id = request.form.get('id')
     print(id)
-    query = ("UPDATE alerts SET completed = 't' WHERE id = (%s);", (id,))
+    query = ("UPDATE alerts SET completed = 't' WHERE id = (%s);", [id])
     databaseResult = executeSingleQuery(query, fetch = True)
 
     
--- web/static/attendance.js
+++ web/static/attendance.js
@@ -1405,8 +1405,7 @@ function checkAlert(id) {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.open("POST", "http://ec2-35-160-216-144.us-west-2.compute.amazonaws.com:5000/checkAlert");
     xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
-    xmlhttp.send(JSON.stringify(id));
-    console.log(id);
+    xmlhttp.send("id=" + id);
     closeAlert();
     return "";
 }

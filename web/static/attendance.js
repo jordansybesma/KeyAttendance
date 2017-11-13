b737fe151f200310581e71300e891261920efb88
festeringalfalfah <festeringalfalfa@bitbucket.org>
2017-11-13 16:08:35 -0600

    Trying to implement adding an alert

--- web/static/attendance.js
+++ web/static/attendance.js
@@ -1413,7 +1413,7 @@ function checkAlert(id) {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.open("POST", "http://ec2-35-160-216-144.us-west-2.compute.amazonaws.com:5000/checkAlert/");
     xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
-    xmlhttp.send("id=" + spock);
+    xmlhttp.send("id=" + id);
     closeAlert();
 }
 

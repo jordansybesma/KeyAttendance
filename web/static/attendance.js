0850a3aa87a2478c544289166700c8886315909c
festeringalfalfah <festeringalfalfa@bitbucket.org>
2018-01-04 14:47:25 -0600

    working to make names in attendance sheet link to profiles

--- web/static/attendance.js
+++ web/static/attendance.js
@@ -828,7 +828,7 @@ function preprocessAddAttendant(fullName){
 //    var fullName = first + " " + last;
     var fields = ['art','madeFood','recievedFood','leadership','exersize','mentalHealth','volunteering','oneOnOne'];
     var row = table.insertRow(1);
-    row.insertCell(0).innerHTML = fullName;
+    row.insertCell(0).innerHTML = "WOAH";
 
     for(var i = 0; i < 8; i++)  {
         var str = "<input type=\"checkbox\" onclick=\"selectActivity('" + fullName + "','" + fields[i] + "', '" + date + "')\">";

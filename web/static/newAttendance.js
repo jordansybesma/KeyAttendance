bd8cb3ccef3262573da646403b0dbf170c8ee68e
rachelmoore212 <moorer2@carleton.edu>
2018-02-15 14:20:43 -0600

    working on download

--- web/static/newAttendance.js
+++ web/static/newAttendance.js
@@ -1248,7 +1248,7 @@ function createFileHelper(_, attendance) {
     console.log(columns);
     var nameRow = [];
     for (i in columns) {
-        if (columns[i][1] == true) {
+        if (columns[i][1]) {
             console.log(columns[i][2]);
             nameRow.push = columns[i][2];
         }

3f990307424adbe04965f8671d3823eca7996f1c
Russell Smith <Russll@eduroam-160-74.dyn.carleton.edu>
2018-02-22 14:32:07 -0600

    upload temp

--- web/static/newAttendance.js
+++ web/static/newAttendance.js
@@ -821,17 +821,7 @@ function placeStudentPicture(_, data) {
 }
 
 function uploadPicture() {
-  var http = require('http');
-  var formidable = require('formidable');
-
-  http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<input type="submit">');
-    res.write('</form>');
-    return res.end();
-}).listen(8080);
+  console.log("Clicked uploadPicture!")
 }
 
 // SP

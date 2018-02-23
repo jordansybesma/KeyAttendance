89ec6fd8e26ee71256b397e780d78fe56c4f5126
rachelmoore212 <moorer2@carleton.edu>
2018-02-23 15:13:35 -0600

    finishing touches

--- web/flaskEnd.py
+++ web/flaskEnd.py
@@ -1,4 +1,4 @@
-import newBackEnd
+import backEnd
 import flask
 from flask import request
 import sys
@@ -24,149 +24,149 @@ def main():
 
 @app.route('/addText/', methods=['POST'])
 def foo():
-    return newBackEnd.foo(request)
+    return backEnd.foo(request)
 
 
 @app.route('/getReports')
 def getReports():
-    return newBackEnd.getReports()
+    return backEnd.getReports()
 
 @app.route('/createAttendanceData/', methods = ["POST"])
 def createAttendanceData():
-    return newBackEnd.createAttendanceData(request)
+    return backEnd.createAttendanceData(request)
 
 @app.route('/addNewStudent/', methods = ["POST"])
 def addNewStudent():
-    return newBackEnd.addNewStudent(request)
+    return backEnd.addNewStudent(request)
 
 @app.route('/updateStudentInfo/', methods = ["POST"])
 def updateStudentInfo():
-    return newBackEnd.updateStudentInfo(request)
+    return backEnd.updateStudentInfo(request)
 
 @app.route('/getStudentInfo/<name>')
 def getStudentInfo(name):
-    return newBackEnd.getStudentInfo(name)
+    return backEnd.getStudentInfo(name)
 
 @app.route('/addStudentColumn', methods = ["POST"])
 def addStudentColumn():
-    return newBackEnd.addStudentColumn(request)
+    return backEnd.addStudentColumn(request)
 
 @app.route('/alterStudentColumn', methods = ["POST"])
 def alterStudentColumn():
-    return newBackEnd.alterStudentColumn(request)
+    return backEnd.alterStudentColumn(request)
 
 @app.route('/deleteStudentColumn', methods = ["POST"])
 def deleteStudentColumn():
-    return newBackEnd.deleteStudentColumn(request)
+    return backEnd.deleteStudentColumn(request)
 
 @app.route('/getStudentColumns')
 def getStudentColumns():
-    return newBackEnd.getStudentColumns()
+    return backEnd.getStudentColumns()
 
 @app.route('/sendFeedback', methods = ["POST"])
 def sendFeedback():
-    return newBackEnd.sendFeedback(request)
+    return backEnd.sendFeedback(request)
 
 @app.route('/getAttendance/<date>')
 def getAttendance(date):
-    return newBackEnd.getAttendance(date)
+    return backEnd.getAttendance(date)
 
 @app.route('/getLogin/<login>')
 def getLogin(login):
-    return newBackEnd.getLogin(login)
+    return backEnd.getLogin(login)
 
 @app.route('/getStudentAttendance/<student>/')
 def getStudentAttendance(student):
-    return newBackEnd.getStudentAttendance(student)
+    return backEnd.getStudentAttendance(student)
 
 @app.route('/getMasterAttendance')
 def getMasterAttendance():
-    return newBackEnd.getMasterAttendance()
+    return backEnd.getMasterAttendance()
 
 @app.route('/addAttendanceColumn', methods = ["POST"])
 def addAttendanceColumn():
-    return newBackEnd.addAttendanceColumn(request)
+    return backEnd.addAttendanceColumn(request)
 
 @app.route('/deleteAttendanceColumn', methods = ["POST"])
 def deleteAttendanceColumn():
-    return newBackEnd.deleteAttendanceColumn(request)
+    return backEnd.deleteAttendanceColumn(request)
 
 @app.route('/updateAttendanceColumn', methods = ["POST"])
 def updateAttendanceColumn():
-    return newBackEnd.updateAttendanceColumn(request)
+    return backEnd.updateAttendanceColumn(request)
 
 @app.route('/getAttendanceColumns')
 def getAttendanceColumns():
-    return newBackEnd.getAttendanceColumns()
+    return backEnd.getAttendanceColumns()
 
 @app.route('/moveAttendanceColumnUp', methods = ["POST"])
 def moveAttendanceColumnUp():
-    return newBackEnd.moveAttendanceColumnUp(request)
+    return backEnd.moveAttendanceColumnUp(request)
 
 @app.route('/getMasterAttendanceDate/<dates>')
 def getMasterAttendanceDate(dates):
-    return newBackEnd.getMasterAttendanceDate(dates)
+    return backEnd.getMasterAttendanceDate(dates)
 
 @app.route('/deleteAttendant', methods = ["POST"])
 def deleteAttendant():
-    return newBackEnd.deleteAttendant(request)
+    return backEnd.deleteAttendant(request)
 
 @app.route('/getDates')
 def getDates():
-    return newBackEnd.getDates()
+    return backEnd.getDates()
 
 @app.route('/selectActivity', methods = ["POST"])
 def selectActivity():
-    return newBackEnd.selectActivity(request)
+    return backEnd.selectActivity(request)
 
 @app.route('/addAttendant/', methods = ["POST"])
 def addAttendant():
-    return newBackEnd.addAttendant(request)
+    return backEnd.addAttendant(request)
 
 @app.route('/getNumberAttended/<string>')
 def getNumberAttended(string):
-    return newBackEnd.getNumberAttended(string)
+    return backEnd.getNumberAttended(string)
 
 
 @app.route('/autofill/<partialString>')
 def autofill(partialString):
-    return newBackEnd.autofill(partialString)
+    return backEnd.autofill(partialString)
 
 @app.route('/frequentPeers/<string>')
 def frequentPeers(string):
-    return newBackEnd.frequentPeers(string)
+    return backEnd.frequentPeers(string)
 
 @app.route('/studentProfile/<string>')
 def studentProfile(string):
-    return newBackEnd.studentProfile(string)
+    return backEnd.studentProfile(string)
 
 @app.route('/getID/<string>')
 def getStudentID(string):
-    return newBackEnd.getStudentID(string)
+    return backEnd.getStudentID(string)
 
 @app.route('/getStudentByID/<string>')
 def getStudentByID(string):
-    return newBackEnd.getStudentByID(string)
+    return backEnd.getStudentByID(string)
 
 @app.route('/getJustID/<string>')
 def getJustID(string):
-    return newBackEnd.getJustID(string)
+    return backEnd.getJustID(string)
 
 @app.route('/getAlerts')
 def getAlerts():
-    return newBackEnd.getAlerts()
+    return backEnd.getAlerts()
 
 @app.route('/addAlert/', methods = ["POST"])
 def addAlert():
-    return newBackEnd.addAlert(request)
+    return backEnd.addAlert(request)
 
 @app.route('/checkAlert/', methods = ["POST"])
 def checkAlert():
-    return newBackEnd.checkAlert(request)
+    return backEnd.checkAlert(request)
 
 @app.route('/getPhoto/<string>')
 def getPhoto(string):
-    return newBackEnd.getPhoto(string)
+    return backEnd.getPhoto(string)
 
 
 if __name__ == "__main__":

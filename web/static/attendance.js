4146d01ec90beac872fab3fe3284657520d23b76
Lindsey Chavez <lindseychavez@eduroam-164-78.dyn.carleton.edu>
2018-02-24 20:04:14 -0600

    DELETED FEEDBACK more2

--- web/static/attendance.js
+++ web/static/attendance.js
@@ -1736,19 +1736,6 @@ function exportToCsv(filename, rows) {
     }
 }
 
-// FF
-// Sends input in feedback textbox to database.
-/* 
-function sendFeedback() {
-    var feedback = document.getElementById("feedback").value;
-    var date = getCurrentDate();
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("POST", urlBase + "/sendFeedback");
-    xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
-    xmlhttp.send("date=" + date + "&feedback=" + feedback);
-    document.getElementById("feedback").value = "";
-}
- */
 
 // AS
 // Fills the code text box under the table in an attendance sheet.

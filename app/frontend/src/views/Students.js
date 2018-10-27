10955421501e1115c33d9442d0f02813406b9eda
Martin Hoffman <martin.c.hoffman@gmail.com>
2018-10-27 15:35:42 -0500

    Added WIP Search Functionality

--- app/frontend/src/views/Students.js
+++ app/frontend/src/views/Students.js
@@ -1,34 +1,72 @@
 import React, { Component } from 'react';
+import Autocomplete from '../components/Autocomplete';
 import { Label } from 'react-bootstrap';
 
 class Students extends Component {
   state = {
-    key: []
+    key: [],
+    mode: 'search'
   }
 
   async componentDidMount() {
     try {
       var id = window.location.href.replace("http://localhost:3000/students/", "");   // This feels wrong...
-      const res = await fetch('http://127.0.0.1:8000/api/students/' + id);
-      const key = await res.json();
-      this.setState({
-        key
-      });
+      if (id) {
+        const res = await fetch('http://127.0.0.1:8000/api/students/' + id);
+        const key = await res.json();
+        this.setState({
+          key,
+          mode: 'display'
+        });
+      } else {
+        const res = await fetch('http://127.0.0.1:8000/api/');
+        var key = await res.json();
+        key = this.makeSuggestionsArray(key);
+        this.setState({
+          key,
+          mode: 'search'}) 
+      }
+
+
     } catch (e) {
       console.log(e);
     }
   }
 
+  makeSuggestionsArray(key) {
+    var array = [];
+
+    for (var object in key) {
+      var fullName = (key[object]['first_name'] + ' ' + key[object]['last_name']);
+      array.push(fullName);
+    }
+    console.log(array);
+    return array;
+  }
+
   render() {
-    return (
-      <div className='content'>
-        <h1> Key Students </h1>
+    if(this.state.mode === "search"){
+      return (
+        <div className='content'>
+          <h1> Key Students </h1>
+          <Autocomplete
+            suggestions={this.state.key}
+          />
+        </div>
+      );
+    }else{
+      return (
+        <div className='content'>
+        <h1> Student Profile </h1>
+        <div>
           Name: {this.state.key.first_name} {this.state.key.last_name} <br />
           ID: <Label>{this.state.key.id}</Label> <br />
           First Attendance: {this.state.key.first_attendance} <br />
           Number of Visits: {this.state.key.number_visits}
+        </div>
       </div>
-    );
+      );
+    }
   }
 }
 

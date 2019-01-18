40b43b26eb131bcaf3b46c2d991c2769fbb83864
jordon okelly <jordonokelly@jordons-mbp.home>
2019-01-17 20:13:22 -0600

    changes to student page

--- app/frontend/src/views/Students.js
+++ app/frontend/src/views/Students.js
@@ -13,6 +13,7 @@ class Students extends Component {
       id: null,
       profileData: {}
     };
+    this.edit = this.edit.bind(this);
     this.handler = this.handler.bind(this);
   }
 
@@ -81,6 +82,28 @@ class Students extends Component {
       console.log(e);
     }
   }
+  
+  edit() {
+    this.setState({mode: 'edit'})
+  }
+  
+  handleChange(evt) {
+    console.log(this.state.profileData);
+    this.setState({
+      first_name: evt.target.value
+    });
+    console.log(this.state.profileData);
+  }
+  
+  handleSubmit(evt) {
+    var object = {};
+    console.log(this);
+    //object
+    this.setState({
+      first_name: evt.target.value
+    });
+    
+  }
 
   render() {
     if (this.state.mode === 'search') {
@@ -111,7 +134,7 @@ class Students extends Component {
 					handler={this.handler}
 				  />
 			  </div>
-			  <div className='col-md-8 top-bottom-padding'>
+          <div className='col-md-8 top-bottom-padding'>
 				Name: {this.state.profileData.first_name} {this.state.profileData.last_name} <br/>
                 ID: <Label>N/A</Label> <br/>
                 Birthday: xx/xx/xxxx <br/>
@@ -119,7 +142,40 @@ class Students extends Component {
                 Gender: N/A <br/>
                 First Attendance: {this.state.profileData.first_attendance} <br/>
                 Number of Visits: {this.state.profileData.number_visits} <br/>
+                <button onClick={this.edit}>
+                  Edit
+                </button>
 			  </div>
+        	</div>
+		  </div>
+		</div>
+      );
+    }
+    else if (this.state.mode === 'edit') {
+      return (
+        <div className='content'>
+          <h1> Student Profile </h1>
+		  <div className='container-fluid no-padding'>
+  			<div className='row justify-content-start'>
+			  <div className='col-md-4 to-front top-bottom-padding'>
+				  <Autocomplete
+					suggestions={this.state.suggestionsArray}
+					handler={this.handler}
+				  />
+			  </div>
+          <div className='col-md-8 top-bottom-padding'>
+              <form className='col-md-8 top-bottom-padding' onSubmit={this.handleSubmit}>
+              First Name: <input type="text" defaultValue={this.state.profileData.first_name} onChange={evt => this.handleChange(evt)} /> <br/>
+              Last Name: <input type="text" defaultValue={this.state.profileData.last_name} onChange={this.handleChange} /> <br/>
+              ID: <input type="text" defaultValue="N/A" onChange={this.handleChange} /> <br/>
+              Birthday: <input type="text" defaultValue="xx/xx/xxxx" onChange={this.handleChange} /> <br/>
+              Nickname: <input type="text" defaultValue="N/A" onChange={this.handleChange} /> <br/>
+              Gender: <input type="text" defaultValue="N/A" onChange={this.handleChange} /> <br/>
+              First Attendance: <input type="text" defaultValue={this.state.profileData.first_attendance} onChange={this.handleChange} /> <br/>
+              Number of Visits: <input type="text" defaultValue={this.state.profileData.number_visits} onChange={this.handleChange} /> <br/>
+              <input type="submit" value="Submit" />
+              </form>
+          </div>
         	</div>
 		  </div>
 		</div>

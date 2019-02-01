8b1508b3d2cfa812330fab13eedc0a323ee060fc
Jordan <sybesmaj@carleton.edu>
2019-02-01 15:00:02 -0600

    i think i fixed the file structure

---
+++ app/backend/build/precache-manifest.0d7846d769d5e873529f57235d565e2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a68170ed3549b1e8b143",
+    "url": "/static/js/main.a68170ed.chunk.js"
+  },
+  {
+    "revision": "91b7dcb9747d3de8346c",
+    "url": "/static/js/1.91b7dcb9.chunk.js"
+  },
+  {
+    "revision": "a68170ed3549b1e8b143",
+    "url": "/static/css/main.bb12c3e4.chunk.css"
+  },
+  {
+    "revision": "eb0c2b465632c1a3cf6dde2f5cb99c12",
+    "url": "/index.html"
+  }
+];

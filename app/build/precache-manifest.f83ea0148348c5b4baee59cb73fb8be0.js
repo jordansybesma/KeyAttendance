208f78c8344121b4d008e6b949d16c05a9f2c942
Jordan <sybesmaj@carleton.edu>
2019-02-01 13:26:20 -0600

    adding build files because the server doesnt like 'npm install'

---
+++ app/build/precache-manifest.f83ea0148348c5b4baee59cb73fb8be0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "733dd016f0183ce5edbc",
+    "url": "/static/js/main.733dd016.chunk.js"
+  },
+  {
+    "revision": "91b7dcb9747d3de8346c",
+    "url": "/static/js/1.91b7dcb9.chunk.js"
+  },
+  {
+    "revision": "733dd016f0183ce5edbc",
+    "url": "/static/css/main.1baeaabc.chunk.css"
+  },
+  {
+    "revision": "eb622dcc86862699370d3f84968e7dc3",
+    "url": "/index.html"
+  }
+];

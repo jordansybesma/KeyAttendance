7e206e7494141cc12c1a25c00ab79f0953432438
Tina Liu <tina.liu000@gmail.com>
2019-02-08 14:42:05 -0600

    Remove unneeded imports

--- app/backend/key/views/history.py
+++ app/backend/key/views/history.py
@@ -1,10 +1,7 @@
-from django.http import HttpResponseRedirect
 from ..models import AttendanceItems, Students, Activity
-from simple_history.models import HistoricalRecords
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from ..serializers import GroupSerializer, HistoricalRecordSerializer
 
 class History(APIView):
 

2772c482fa6334c402f8161b84868a7f1f4aee3d
Tina Liu <tina.liu000@gmail.com>
2019-02-18 01:02:45 -0600

    Update pipfile.lock, fix error caused by merging

--- app/backend/key/urls.py
+++ app/backend/key/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('users/', users.Users.as_view()),
     path('groups/', groups.Groups.as_view()),
     path('permissions/', permissions.Permissions.as_view()),
-    path('history/', history.History.as_view())
+    path('history/', history.History.as_view()),
     re_path(r'^reports/(?P<vizType>\w*)/$', reports.Reports.as_view()),
     #above regex maps to: /api/reports/vizType/?arg1=blah&arg2=blah
 ]

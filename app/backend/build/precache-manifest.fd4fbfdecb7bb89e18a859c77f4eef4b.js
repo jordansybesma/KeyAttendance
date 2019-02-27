abb31beee7533171c9c5968fe0476a3e3f8cae89
Tina Liu <tina.liu000@gmail.com>
2019-02-27 15:04:04 -0600

    Update build folder

---
+++ app/backend/build/precache-manifest.fd4fbfdecb7bb89e18a859c77f4eef4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a344a6c7e456a104fac9b9c94b4c530",
+    "url": "/static/media/blank_profile_pic.9a344a6c.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0c4ec143e831af61fc70",
+    "url": "/static/js/main.0c4ec143.chunk.js"
+  },
+  {
+    "revision": "f3dcf3a704ba7900b820",
+    "url": "/static/js/1.f3dcf3a7.chunk.js"
+  },
+  {
+    "revision": "0c4ec143e831af61fc70",
+    "url": "/static/css/main.3a5b56d7.chunk.css"
+  },
+  {
+    "revision": "674b4a82918d42ee127eff5367837d42",
+    "url": "/index.html"
+  }
+];
--- app/backend/build/service-worker.js
+++ app/backend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.26f2e96429066230c4f92109277a265d.js"
+  "/precache-manifest.fd4fbfdecb7bb89e18a859c77f4eef4b.js"
 );
 
 workbox.clientsClaim();

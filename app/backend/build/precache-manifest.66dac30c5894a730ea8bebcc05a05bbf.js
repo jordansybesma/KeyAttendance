2a760110a2292dbb49509d2b6e396787bc7d6fdf
Jordan Sybesma <sybesmaj@carleton.edu>
2019-03-02 03:31:57 -0600

    updated build folder

---
+++ app/backend/build/precache-manifest.66dac30c5894a730ea8bebcc05a05bbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a344a6c7e456a104fac9b9c94b4c530",
+    "url": "/static/media/blank_profile_pic.9a344a6c.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5a032246a122cd9613be",
+    "url": "/static/js/main.5a032246.chunk.js"
+  },
+  {
+    "revision": "11a47c71c058f035dc53",
+    "url": "/static/js/1.11a47c71.chunk.js"
+  },
+  {
+    "revision": "5a032246a122cd9613be",
+    "url": "/static/css/main.3622529c.chunk.css"
+  },
+  {
+    "revision": "36b4bc476a8ab9ee6da1cf6b984ef06c",
+    "url": "/index.html"
+  }
+];
--- app/backend/build/service-worker.js
+++ app/backend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.834f97da859d7077ea822d04ce6b9015.js"
+  "/precache-manifest.66dac30c5894a730ea8bebcc05a05bbf.js"
 );
 
 workbox.clientsClaim();

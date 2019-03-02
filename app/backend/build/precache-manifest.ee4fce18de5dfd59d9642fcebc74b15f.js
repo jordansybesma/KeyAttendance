9ebbc0d34033b3b42a9dcb0c625a7fc4d31001a5
Jordan Sybesma <sybesmaj@carleton.edu>
2019-03-02 03:35:00 -0600

    forgot to switch domains whoops

--- app/backend/build/precache-manifest.ee4fce18de5dfd59d9642fcebc74b15f.js
+++ app/backend/build/precache-manifest.ee4fce18de5dfd59d9642fcebc74b15f.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "5a032246a122cd9613be",
-    "url": "/static/js/main.5a032246.chunk.js"
+    "revision": "c2fa949ddfc1a5469cda",
+    "url": "/static/js/main.c2fa949d.chunk.js"
   },
   {
     "revision": "11a47c71c058f035dc53",
     "url": "/static/js/1.11a47c71.chunk.js"
   },
   {
-    "revision": "5a032246a122cd9613be",
+    "revision": "c2fa949ddfc1a5469cda",
     "url": "/static/css/main.3622529c.chunk.css"
   },
   {
-    "revision": "36b4bc476a8ab9ee6da1cf6b984ef06c",
+    "revision": "faf49500f0c4c01a22de93877289eff9",
     "url": "/index.html"
   }
 ];
--- app/backend/build/service-worker.js
+++ app/backend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.66dac30c5894a730ea8bebcc05a05bbf.js"
+  "/precache-manifest.ee4fce18de5dfd59d9642fcebc74b15f.js"
 );
 
 workbox.clientsClaim();

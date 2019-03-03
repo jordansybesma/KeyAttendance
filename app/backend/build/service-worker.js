24d3a06b7760e00aa06458aa946cefdaaa28c7cc
Tina Liu <tina.liu000@gmail.com>
2019-03-02 20:19:53 -0600

    Update build

--- app/backend/build/service-worker.js
+++ app/backend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1d40592baacd2bda8dd305035512f678.js"
+  "/precache-manifest.c6843758f8646025d9b18574851a99fa.js"
 );
 
 workbox.clientsClaim();
